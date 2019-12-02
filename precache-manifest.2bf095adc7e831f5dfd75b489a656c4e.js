e2f95fc8584281d0664d49ecf0cb49d393f8eb72
desireemoon <desiree.dewysocki@gmail.com>
2019-12-02 01:05:31 -0500

    Updates

--- precache-manifest.2bf095adc7e831f5dfd75b489a656c4e.js
+++ precache-manifest.2bf095adc7e831f5dfd75b489a656c4e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9b6f3df53febf7fb8ce072f83fc4604",
+    "revision": "1742024afc23cd5ba75b6d8206ce65a0",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "9e40866f62542a4ab35e",
-    "url": "/portfolio/static/css/main.55710f18.chunk.css"
+    "revision": "d2211f5d0cf5a3b07cf5",
+    "url": "/portfolio/static/css/main.b55e6deb.chunk.css"
   },
   {
     "revision": "45b80fe3ec48fb15ded2",
     "url": "/portfolio/static/js/2.6bc29d64.chunk.js"
   },
   {
-    "revision": "9e40866f62542a4ab35e",
-    "url": "/portfolio/static/js/main.18beacac.chunk.js"
+    "revision": "d2211f5d0cf5a3b07cf5",
+    "url": "/portfolio/static/js/main.fb1861cf.chunk.js"
   },
   {
     "revision": "1f6a8d5fcc0eb02bac45",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e8ad9183944d7cf6b02af4a1c107900a.js"
+  "/portfolio/precache-manifest.2bf095adc7e831f5dfd75b489a656c4e.js"
 );
 
 self.addEventListener('message', (event) => {
