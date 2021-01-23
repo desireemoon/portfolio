059cb94f084c3d2f7edb1af6fc9c566576d45f0c
desireemoon <desiree.dewysocki@gmail.com>
2021-01-23 18:51:08 -0500

    updated About Me

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                     <img className="prof-pic" src="https://res.cloudinary.com/dsnhxcw0e/image/upload/v1575234478/Profile-photo_siez95.jpg" alt="Photo of Desiree" />
                 </div>
                 <div className="bio">
-                    <p>As a creative and enthusiastic full stack developer, I'm passionate about building high-quality, user-friendly applications. My background in the arts and marketing provides me with unique insights into the customer experience, which enables me to design, develop, and present to a diverse audience. </p>
+                    <p>As a creative and enthusiastic full stack developer, I'm passionate about building high-quality, user-friendly applications. My background in marketing provides me with unique insights into the customer experience, enabling me to design, develop, and present to a diverse audience. </p>
                     <p>Currently, I work as a technical project manager and software engineer for a cybersecurity company, which specializes in application security and reverse engineering services. I manage the end-to-end engagement life-cycle for our consultants and clients, while utilizing my programming background to automate and optimize our processes and platforms.</p> 
                     <p>I’m interested in challenging and rewarding opportunities that empower and foster stewardship, where I can cultivate my skills and thrive as a software developer.</p>
                 </div>
